e1d5beb93d556d7f304f952ac45ebd8ab2b21852
Gor777777777 <gorgoar@gmail.com>
2017-01-16 22:42:19 +0400

    Made changes

---
+++ Polidrom/PolidromExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Polidrom
+{
+    public static class PolidromExtension
+    {
+        public static bool IsPolidrom(this string IsPolidrom)
+        {
+            string conversely = "";
+            string outputText = "";
+
+            for (int i = 0; i < IsPolidrom.Length; i++)
+            {
+                if (Char.IsLetter(IsPolidrom, i))
+                    outputText += IsPolidrom[i];
+            }
+            outputText = outputText.ToLower();
+
+            for (int i = outputText.Length - 1; i >= 0; i--)
+            {
+                conversely += outputText[i];
+            }
+            if (outputText == conversely)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Polidrom/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Polidrom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Polidrom")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("089f1cbb-7eb3-4fb2-875e-e6a8f7dac9df")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
